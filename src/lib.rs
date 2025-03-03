185b9518d396169d67b7cf7af939ff2160a0c857
Evgeniy Zakharov <eugenezakharov7@gmail.com>
2025-03-04 00:02:46 +0300

    Add lib summary

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! # MQTT Protocol Implementation in Rust
+//!
+//! mqute-codec crate provides an implementation of the MQTT protocol in Rust.
+//! It includes modules for encoding, decoding, and handling MQTT packets.
+
 pub mod codec;
 mod error;
 pub mod protocol;
